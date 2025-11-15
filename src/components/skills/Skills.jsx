d79d15c48de4766fc30b77ec69cb1024eeaa5a17
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-15 14:43:16 +0200

    Remove certificates and re arrange education

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -6,41 +6,25 @@ const Skills = ({ ref }) => {
     const skills = [
         {
             id: 'skill-1',
-            year: '2021',
-            title: 'Matric',
-            place: 'Mandisa Shiceka',
-            description:
-                'Matriculated with a distinction average. The stream was Mathematics and Science',
-        },
-        {
-            id: 'skill-2',
-            year: '2026-Expected',
+            year: '2023 - Present',
             title: 'BSc Computer Science and Computational Applications',
             place: 'Wits University',
             description:
                 'Expected to get this degree on the specified above year. Relavant course work includes Software Design',
         },
         {
-            id: 'skill-3',
-            year: '2025',
-            title: 'Software Development',
-            place: 'Udemy',
+            id: 'skill-2',
+            year: '2021',
+            title: 'Matric',
+            place: 'Mandisa Shiceka',
             description:
-                'Completed 50+ hours of coursework covering Spring, Spring Boot, Spring Data JPA, Spring Security and Spring MVC',
-        },
-        {
-            id: 'skill-4',
-            year: '2025',
-            title: 'Job Simulation',
-            place: 'JP Morgan Chase',
-            description:
-                'Obtained a certificate of completion of job simulation covering software developement with Spring, Spring Boot and MySql',
+                'Matriculated with a distinction average. The stream was Mathematics and Science',
         },
     ];
 
     return (
         <section className="skills" ref={ref}>
-            <h2>Education & Certifications</h2>
+            <h2>Education</h2>
             <Marker />
             <section className="skills-info">
                 <div className="skills-info__divider"></div>
