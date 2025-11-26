6690152a7387c720e6c20801ebd726ed2ab1629c
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-26 14:40:17 +0200

    Implement form submission

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import Marker from '../../utils/Marker.jsx';
 import ContactDetails from './ContactDetails.jsx';
 import emailImg from '../../assets/email.png';
@@ -7,8 +9,52 @@ import ContactForm from './ContactForm.jsx';
 import './Contact.css';
 
 const Contact = ({ ref }) => {
-    const handleSubmit = (event) => {
+    const [data, setData] = useState({ name: '', email: '', message: '' });
+    const [error, setError] = useState();
+    const [loading, setLoading] = useState();
+    const [success, setSuccess] = useState();
+
+    // Function that handles input field change
+    const handleChange = (field, value) => {
+        // Reset the error and success states
+        setError('');
+        setSuccess('');
+
+        // Set the data. field refers to the data field we are attempting to chane
+        setData((prevData) => {
+            // Make a deep copy the data
+            const newData = { ...prevData };
+
+            // Set the field accordingly and return the newly created data
+            newData[field.toLowerCase()] = value;
+            return newData;
+        });
+    };
+
+    const handleSubmit = async (event) => {
         event.preventDefault();
+        try {
+            setLoading(true);
+            const baseUrl = import.meta.env.VITE_BACKEND_BASE_URL;
+            const response = await fetch(baseUrl + '/sendInfo', {
+                method: 'POST',
+                body: JSON.stringify(data),
+            });
+
+            const result = await response.json();
+            setLoading(false);
+
+            if (!response.ok) {
+                setError(result.message);
+            }
+
+            setSuccess(result.message);
+            setError('buff');
+        } catch (error) {
+            setLoading(false);
+            console.error(error);
+            setError('Something went really bad! Please try again later');
+        }
     };
 
     const contactFields = [
@@ -22,10 +68,6 @@ const Contact = ({ ref }) => {
         },
         {
             id: 'field-3',
-            value: 'Subject',
-        },
-        {
-            id: 'field-4',
             value: 'Message',
         },
     ];
@@ -54,7 +96,14 @@ const Contact = ({ ref }) => {
             <Marker />
             <div className="details-container">
                 <ContactDetails details={personalDetails} />
-                <ContactForm fields={contactFields} onSubmit={handleSubmit} />
+                <ContactForm
+                    success={success}
+                    loading={loading}
+                    error={error}
+                    fields={contactFields}
+                    onSubmit={handleSubmit}
+                    onChange={handleChange}
+                />
             </div>
         </section>
     );
