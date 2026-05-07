97dad147a01ef93b50436c9b9e7c635268f230f3
tlhalesebaeng <tlhalesebaeng@gmail.com>
2026-05-07 11:17:03 +0200

    Add second experience

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -11,6 +11,14 @@ const Experience = ({ ref }) => {
             description:
                 'I currently tutor first-year Computer Science students in Introduction to Algorithms and Programming (IAP) and Basic Computer Organisations (BCO)',
         },
+        {
+            id: 'experience-2',
+            year: 'May 2026 - Present',
+            title: 'Peer Coding Tutor',
+            place: 'Wits University',
+            description:
+                'I assist Computer Science first year students with understanding advanced programming concepts by hosting three-hour sessions on Saturdays. This program is facilitated by the Wits Computer Science and Applied Mathematics school in collaboration with PSG.',
+        },
     ];
     return <SeparatedList ref={ref} infoClass="experience-info" title="Experience" list={experienceList} />;
 };
