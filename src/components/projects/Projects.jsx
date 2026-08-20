a2f6e4991a71745f9cc26208fb84a1a0ccce4038
tlhalesebaeng <tlhalesebaeng@gmail.com>
2026-08-20 11:12:19 +0200

    Include latest projects

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -3,65 +3,58 @@ import ProjectItem from './ProjectItem.jsx';
 import './Projects.css';
 
 const Projects = ({ ref }) => {
-    const projects = [
-        {
-            id: 'project-1',
-            name: 'Test Maker',
-            desc: 'A platform to create tests and invite others to take them in a fun and interactive way.',
-            techStack: [
-                { id: 'project-1-stack-1', name: 'Java' },
-                { id: 'project-1-stack-2', name: 'Spring Boot' },
-                { id: 'project-1-stack-3', name: 'Hibernate' },
-                { id: 'project-2-stack-4', name: 'Maven' },
-                { id: 'project-1-stack-5', name: 'MySQL' },
-                { id: 'project-2-stack-6', name: 'React.js' },
-                { id: 'project-2-stack-7', name: 'CSS' },
-            ],
-            link: 'https://github.com/tlhalesebaeng/testmaker-frontend',
-        },
-        {
-            id: 'project-2',
-            name: 'League Oracle',
-            desc: 'A full‑stack dynamic web application that enables users to seamlessly manage soccer leagues.',
-            techStack: [
-                { id: 'project-1-stack-1', name: 'Java' },
-                { id: 'project-1-stack-2', name: 'Spring Boot' },
-                { id: 'project-1-stack-3', name: 'Hibernate' },
-                { id: 'project-2-stack-4', name: 'Maven' },
-                { id: 'project-1-stack-5', name: 'MySQL' },
-                { id: 'project-2-stack-6', name: 'React.js' },
-                { id: 'project-2-stack-7', name: 'CSS' },
-            ],
-            link: 'https://github.com/tlhalesebaeng/league-oracle',
-        },
-        {
-            id: 'project-3',
-            name: 'Authentication Template',
-            desc: 'A web authentication template with signup, login, and password recovery with JSON Web Tokens.',
-            techStack: [
-                { id: 'project-2-stack-1', name: 'Java' },
-                { id: 'project-2-stack-2', name: 'Spring Boot' },
-                { id: 'project-2-stack-3', name: 'Hibernate' },
-                { id: 'project-2-stack-4', name: 'Maven' },
-                { id: 'project-2-stack-5', name: 'MySQL' },
-                { id: 'project-2-stack-6', name: 'React.js' },
-                { id: 'project-2-stack-7', name: 'CSS' },
-            ],
-            link: 'https://github.com/tlhalesebaeng/auth-template-backend',
-        },
-    ];
+  const projects = [
+    {
+      id: 'project-1',
+      name: 'Tutor Management System',
+      desc: 'Currently building a platform to streamline tutor allocatons for our school',
+      techStack: [
+        { id: 'project-1-stack-1', name: 'Firebase' },
+        { id: 'project-1-stack-2', name: 'Node.js' },
+        { id: 'project-1-stack-3', name: 'Express' },
+        { id: 'project-2-stack-4', name: 'React' },
+        { id: 'project-2-stack-5', name: 'PostgreSQL' },
+      ],
+      link: 'https://github.com/tlhalesebaeng',
+    },
+    {
+      id: 'project-2',
+      name: 'Test Maker',
+      desc: 'A platform to create tests and invite others to take them in a fun and interactive way',
+      techStack: [
+        { id: 'project-1-stack-1', name: 'Spring Boot' },
+        { id: 'project-2-stack-2', name: 'React' },
+        { id: 'project-1-stack-3', name: 'Hibernate' },
+        { id: 'project-1-stack-4', name: 'PostgreSQL' },
+      ],
+      link: 'https://github.com/tlhalesebaeng/testmaker-frontend',
+    },
 
-    return (
-        <section className="projects" ref={ref}>
-            <h2>My Projects</h2>
-            <Marker />
-            <ul className="projects-list">
-                {projects.map((project) => (
-                    <ProjectItem key={project.id} project={project} />
-                ))}
-            </ul>
-        </section>
-    );
+    {
+      id: 'project-3',
+      name: 'SA Learnerships and Skills Development Portal',
+      desc: 'Built a platform that connects work-seekers with SETA-accredited learnerships',
+      techStack: [
+        { id: 'project-2-stack-1', name: 'Node.js' },
+        { id: 'project-2-stack-2', name: 'Express' },
+        { id: 'project-2-stack-3', name: 'React' },
+        { id: 'project-2-stack-4', name: 'MongoDB' },
+      ],
+      link: 'https://github.com/The-Geeks-Wits/sa-learnerships',
+    },
+  ];
+
+  return (
+    <section className="projects" ref={ref}>
+      <h2>My Projects</h2>
+      <Marker />
+      <ul className="projects-list">
+        {projects.map((project) => (
+          <ProjectItem key={project.id} project={project} />
+        ))}
+      </ul>
+    </section>
+  );
 };
 
 export default Projects;
